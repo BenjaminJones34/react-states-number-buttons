62e143d15274e60eefb0cb87b50c60ba93b9155f
Benjamin Jones <benjamin.jones@clifton-asset.co.uk>
2021-09-24 09:46:26 +0100

    Adding files to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0)
+  
+  const handleAdd = () => {
+    setNumber(number + 1)
+  }
+
+  const handleSubtract = () => {
+    setNumber(number - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <button className="button" onClick={handleAdd}>+</button>
+      <Number number={number} />
+      <button className="button" onClick={handleSubtract}>-</button>
     </div>
   );
+}
+
+const Number = (props) => {
+  return (
+    <div className="number">
+      <p>{props.number}</p>
+    </div>
+  )
 }
 
 export default App;
